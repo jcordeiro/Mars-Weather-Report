d22b7021c554a8f6804f7584d5518568e1097e26
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-03-11 00:10:54 -0400

    Added crouton library

--- src/com/cordeirolabs/weatherofmars/MarsWeatherActivity.java
+++ src/com/cordeirolabs/weatherofmars/MarsWeatherActivity.java
@@ -8,13 +8,14 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MarsWeatherActivity extends Activity {
 	private MarsWeatherClient client;
-//	private static MarsWeatherReport latest;
+	//	private static MarsWeatherReport latest;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,51 +32,66 @@ public class MarsWeatherActivity extends Activity {
 
 			@Override
 			public void onSuccess(int code, JSONObject json) {
-				
+
 				System.out.println(json.toString());
 
 				Log.d("MARS!", "THIS RUNS TOO!");
 
-				//				try {
 
 				MarsWeatherReport latest = MarsWeatherReport.fromJson(json);
-				
+
 				Log.d("MARS!", latest.toString());
 
 				updateWeatherView(latest);
 
 				Log.d("MARS!", "THIS RUNS THREE!");
 
-				//				} catch (JSONException e) {
-				//					e.printStackTrace();
-				//				}
-
 			}
 
 			@Override
-			public void onFailure(Throwable arg0, JSONObject arg1) {
-				
-				Log.d("MARS!", "WE FAILED!");
-				
-				arg0.printStackTrace();
-				
-				// TODO Auto-generated method stub
-				super.onFailure(arg0, arg1);
+			public void onFailure ( Throwable e, JSONObject errorResponse ) {
+				String msg = "Object *" + e.toString() + "*" + errorResponse.toString();
+				new AlertDialog.Builder(MarsWeatherActivity.this).setMessage(msg).setCancelable(false).setTitle("1").setPositiveButton("OK", null).create().show();
 			}
+
+			@Override
+			public void onFailure ( Throwable e, JSONArray errorResponse ) {
+				String msg = "Array *" + e.toString() + "*" + errorResponse.toString();
+				new AlertDialog.Builder(MarsWeatherActivity.this).setMessage(msg).setCancelable(false).setTitle("2").setPositiveButton("OK", null).create().show();
+			}
+
+			@Override
+			public void onFailure ( Throwable e, String errorResponse ) {
+				String msg = "String *" + e.toString() + "*" + errorResponse;
+				new AlertDialog.Builder(MarsWeatherActivity.this).setMessage(msg).setCancelable(false).setTitle("3").setPositiveButton("OK", null).create().show();
+			}
+
 		});
 
-	
+
 
 	}
 
-
+	// Update the UI with values from a MarsWeatherReport
 	public void updateWeatherView(MarsWeatherReport report) {
-		
+
 		Log.d("MARS!", "UPDATE!");
 
 		// Get references to the UI widgets
-		TextView tv = (TextView)findViewById(R.id.txtCurrentTemperature);
-		tv.setText(report.getMaxCelsiusTemp() + " C");
+		TextView txtCurrentTemperature = (TextView)findViewById(R.id.txtCurrentTemperature);
+		TextView txtHighTempValue = (TextView)findViewById(R.id.txtHighTempValue);
+		TextView txtLowTempValue = (TextView)findViewById(R.id.txtLowTempValue);
+		TextView txtWeatherStatusValue = (TextView)findViewById(R.id.txtWeatherStatusValue);
+		TextView txtSunriseValue = (TextView)findViewById(R.id.txtSunriseValue);
+		TextView txtSunsetValue = (TextView)findViewById(R.id.txtSunsetValue);
+
+		// Update the UI with the values from the MarsWeatherReport
+		txtCurrentTemperature.setText(report.getMaxCelsiusTemp() + " C");
+		txtHighTempValue.setText(report.getMaxCelsiusTemp() + " C");
+		txtLowTempValue.setText(report.getMinCelsiusTemp() + " C");
+		txtWeatherStatusValue.setText(report.getWeatherStatus());
+		txtSunriseValue.setText(report.getSunrise());
+		txtSunsetValue.setText(report.getSunset());
 
 	}
 
