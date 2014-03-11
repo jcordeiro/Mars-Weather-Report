2a45975d0141aacfb1dad1e715281f4a955a0a8a
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-03-10 22:09:30 -0400

    Changed from a LinearLayout to a RelativeLayout

--- src/com/cordeirolabs/weatherofmars/MarsWeatherActivity.java
+++ src/com/cordeirolabs/weatherofmars/MarsWeatherActivity.java
@@ -5,13 +5,27 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MarsWeatherActivity extends Activity {
+	private MarsWeatherClient client;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mars_weather);
+		
+		client = new MarsWeatherClient();
+		
 	}
 
+	
+	public void updateWeatherView(MarsWeatherReport report) {
+		
+		// Get references to the UI widgets
+		
+		
+		
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
